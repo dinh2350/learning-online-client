12f4f43238a41e42e4eb8d3a58715c176308556e
dinh2350 <hao9x0159@gmail.com>
2020-03-30 20:48:00 +0700

    write api get course information and students information of course

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,8 @@ import "./Header.scss";
 export default function Header(props) {
   useEffect(function() {
     props.actGetCourseListByPaginationAPI();
+    props.actGetCourseInformationAPI();
+    props.actGetStudentInformationOfCourseAPI();
   }, []);
   return <div className="header">component Header</div>;
 }
