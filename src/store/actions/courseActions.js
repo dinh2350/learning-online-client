4137a082147a198e958a2caa6f6a4f45b37b7308
dinh2350 <hao9x0159@gmail.com>
2020-03-30 21:21:35 +0700

    update and delete course

--- src/store/actions/courseActions.js
+++ src/store/actions/courseActions.js
@@ -125,3 +125,33 @@ export const actCreateCourseAPI = course => {
       });
   };
 };
+
+export const actUpdateCourseAPI = course => {
+  const headers = {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWJjMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiR1YiLCJuYmYiOjE1ODU1NzU5MjcsImV4cCI6MTU4NTU3OTUyN30.bSoyDCeLKS8wtrk7Uhm3OfhajaC3M2liA_X1mM0UAyQ`
+  };
+  return dispatch => {
+    api(`CapNhatKhoaHoc`, "PUT", course, headers)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+  };
+};
+
+export const actDeleteCourseAPI = courseCode => {
+  const headers = {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWJjMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiR1YiLCJuYmYiOjE1ODU1NzU5MjcsImV4cCI6MTU4NTU3OTUyN30.bSoyDCeLKS8wtrk7Uhm3OfhajaC3M2liA_X1mM0UAyQ`
+  };
+  return dispatch => {
+    api(`XoaKhoaHoc`, "DELETE", { MaKhoaHoc: courseCode }, headers)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+  };
+};
