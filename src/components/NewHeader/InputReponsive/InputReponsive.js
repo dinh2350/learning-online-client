efb60b814e7a9b5019f766c14c0cc370b74d96cc
NQphuoc <nguyenqp97@gmail.com>
2020-04-03 08:35:57 +0700

    Edit reponsive

--- src/components/NewHeader/InputReponsive/InputReponsive.js
+++ src/components/NewHeader/InputReponsive/InputReponsive.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InputReponsive.scss";
-export default function InputReponsive() {
+import imgNav from "../../../assets/image/img-header/nav.png";
+export default function InputReponsive(props) {
+  const [isSwitch, setisSwitch] = useState(false);
+  const [listSwitch, setlistSwitch] = useState(["Job", "People", "Learning"]);
+  const handlClick = () => {
+    setisSwitch(!isSwitch);
+  };
+  const showSwitch = () => {
+    return (
+      <div className="InputReponsive__switch">
+        <ul className="InputReponsive__switch__list">
+          {listSwitch.map((item, key) => {
+            return (
+              <li key={key} className="InputReponsive__switch__list__item">
+                {" "}
+                <div className="InputReponsive__switch__item">{item}</div>{" "}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  };
   return (
     <div className="InputReponsive">
+      <button onClick={handlClick}>
+        <img src={imgNav} />
+        {isSwitch && showSwitch()}
+      </button>
       <input type="text" placeholder="Search skills ,subject,or sotfware" />
     </div>
   );
